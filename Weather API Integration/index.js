38de78c62ae8111c8c34b395832ea3096afa1334
abtaaahi <abtahimahib@gmail.com>
2024-08-21 16:57:06 +0600

    new project

---
+++ Weather API Integration/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+app.get('/weather', async (req, res) => {
+    const city = req.query.city || 'London'
+    try{
+        const response = await axios.get('http://api.openweathermap.org/data/2.5/weather', {
+            params: {
+                q: city,
+                appid: 'api-key'
+            }
+        })
+        res.json(response.data)
+    } catch(error) {
+        res.status(500).json({ message: 'Error fetching weather data'})
+    }
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
+})
+
+// default
+// http://localhost:3000/weather
+
+// dynamic
+// http://localhost:3000/weather?city=Paris
